325493b255536cb6af8dc3bfa87584cfa646a3f3
Xiomara Arias <xomita.arias@gmail.com>
2021-05-21 21:24:13 -0500

    CNCO2

--- proyect1.py
+++ proyect1.py
@@ -1,3 +1,5 @@
 "REPOSITORIO 2"
 "LO INTENTADO CASI TODO PARA CONVENCERTE"
 "Mientras el mundo se derrumba aqui a mis pies"
+"Mientras aprendo de esta soledad que desconozcoooo"
+"Me vuelvo a preguntar quizas si sobrevivireee"
